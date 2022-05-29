9c991d9cfba912afed2151109f6bf404d56bbc15
sbahadirm <sbahadirm@gmail.com>
2022-05-29 15:12:39 +0300

    Initial commit

---
+++ src/main/java/com/bahadirmemis/mobileactionbootcamp/MobileActionBootcampApplication.java
@@ -0,0 +1,15 @@
+package com.bahadirmemis.mobileactionbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileActionBootcampApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MobileActionBootcampApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/bahadirmemis/mobileactionbootcamp/MobileActionBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahadirmemis.mobileactionbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileActionBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

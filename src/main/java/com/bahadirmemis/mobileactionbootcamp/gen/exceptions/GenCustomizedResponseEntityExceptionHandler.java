25108feae60b286f044d4acaf75ce8f7d7b53e97
sbahadirm <sbahadirm@gmail.com>
2022-06-05 12:18:20 +0300

    add customized exception response add generic rest response and error response models

---
+++ src/main/java/com/bahadirmemis/mobileactionbootcamp/gen/exceptions/GenCustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.bahadirmemis.mobileactionbootcamp.gen.exceptions;
+
+import com.bahadirmemis.mobileactionbootcamp.gen.response.GenExceptionResponse;
+import com.bahadirmemis.mobileactionbootcamp.gen.response.RestResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+/**
+ * @author Bahadır Memiş
+ * @since 1.0.0
+ */
+@RestController
+@ControllerAdvice
+public class GenCustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest webRequest){
+
+        Date errorDate = new Date();
+        String message = ex.getMessage();
+        String description = webRequest.getDescription(false);
+
+        GenExceptionResponse genExceptionResponse = new GenExceptionResponse(errorDate, message, description);
+
+        RestResponse<GenExceptionResponse> restResponse = RestResponse.error(genExceptionResponse);
+        restResponse.setMessages(message);
+
+        return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}

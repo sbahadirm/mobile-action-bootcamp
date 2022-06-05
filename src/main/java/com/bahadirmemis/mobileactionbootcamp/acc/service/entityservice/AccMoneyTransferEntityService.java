44f9e58aa7e2da2329bf58809079e5980b721e1c
sbahadirm <sbahadirm@gmail.com>
2022-06-05 11:15:12 +0300

    add missed @service annotation

--- src/main/java/com/bahadirmemis/mobileactionbootcamp/acc/service/entityservice/AccMoneyTransferEntityService.java
+++ src/main/java/com/bahadirmemis/mobileactionbootcamp/acc/service/entityservice/AccMoneyTransferEntityService.java
@@ -3,11 +3,13 @@ package com.bahadirmemis.mobileactionbootcamp.acc.service.entityservice;
 import com.bahadirmemis.mobileactionbootcamp.acc.dao.AccMoneyTransferDao;
 import com.bahadirmemis.mobileactionbootcamp.acc.entity.AccMoneyTransfer;
 import com.bahadirmemis.mobileactionbootcamp.gen.service.BaseEntityService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Bahadır Memiş
  * @since 1.0.0
  */
+@Service
 public class AccMoneyTransferEntityService extends BaseEntityService<AccMoneyTransfer, AccMoneyTransferDao> {
 
     public AccMoneyTransferEntityService(AccMoneyTransferDao dao) {
